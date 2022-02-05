80cb9288ea0a410aa0cdfdecea307b854f980bce
Amontes1 <99089388+Amontes1@users.noreply.github.com>
2022-02-05 12:16:46 -0600

    Add files via upload

---
+++ Python/HolaMundo/HolaMundo.py
@@ -0,0 +1,11 @@
+# Comentario: Ejemplo Hola Mundo 
+# Loscomentarios sirven para documentar y aclarar nuestro codigo 
+# los omite o no los toma encuenta en la ejecucion del programa
+
+print(1+2)
+print("Hola Mundo")
+print("Alejandro Montes")
+
+print('Imprimir texto')
+print("""Texto 1""")
+print('''Texto 2''')
